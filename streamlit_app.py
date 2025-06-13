1c236584dc6d2307196f776e5d37c0951c9ea497
banuraghu <mathyraman22@gmail.com>
2025-06-13 12:28:07 -0400

    Update streamlit_app.py
    add cnx variable

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,5 @@
 # Import python packages
 import streamlit as st
-from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
 
 # Write directly to the app
@@ -15,8 +14,8 @@ st.write(
 
 
 
-
-session=get_active_session()
+cnx = st.connect("snowflake")
+session=cnx.session
 my_dataframe = session.table('SMOOTHIES.PUBLIC.FRUIT_OPTIONS').select(col('FRUIT_NAME'))
 
 
