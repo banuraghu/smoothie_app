e485fcb1300eb284fe9ad555bdfacb9ffe205411
banuraghu <mathyraman22@gmail.com>
2025-06-13 21:48:21 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,7 +16,7 @@ st.write(
 
 cnx = st.connection("snowflake")
 session=cnx.session()
-my_dataframe = session.table('SMOOTHIES.PUBLIC.FRUIT_OPTIONS').select(col('SEARCH_ON'))
+my_dataframe = session.table('SMOOTHIES.PUBLIC.FRUIT_OPTIONS').select ((col('FRUIT_NAME'), col('SEARCH_ON'))
 pd_df = my_dataframe.to_pandas()
 
 
